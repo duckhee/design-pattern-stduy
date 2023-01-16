586579c72bed104d4fb5b45d4a4899c1a47b6a33
doukheeWon <fain9301@candh.co.kr>
2023-01-16 10:40:50 +0900

    feat: first commit

---
+++ src/main/java/kr/co/won/designpatternstudy/DesignPatternStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won.designpatternstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/won/designpatternstudy/DesignPatternStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.won.designpatternstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
