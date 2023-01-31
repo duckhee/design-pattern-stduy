535ea58046ddbffdbb7e3e94d3273451c6bdceb1
doukheeWon <fain9301@candh.co.kr>
2023-01-31 17:03:10 +0900

    feat: set adapter pattern structural

---
+++ src/main/java/kr/co/won/designpatternstudy/_06_adapter/_03_java/AdapterInSpring.java
@@ -0,0 +1,14 @@
+package kr.co.won.designpatternstudy._06_adapter._03_java;
+
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+public class AdapterInSpring {
+    public static void main(String[] args) {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        // controller에서 반환을 처리 해주는 handler 이다. -> frontHandler
+        HandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+
+    }
+}
