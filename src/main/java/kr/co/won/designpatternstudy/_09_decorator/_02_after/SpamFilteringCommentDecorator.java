351dd7d21a9511afa954f13a75a0563722496f6a
doukheeWon <fain9301@candh.co.kr>
2023-02-06 11:17:34 +0900

    feat: set decorator pattern

---
+++ src/main/java/kr/co/won/designpatternstudy/_09_decorator/_02_after/SpamFilteringCommentDecorator.java
@@ -0,0 +1,18 @@
+package kr.co.won.designpatternstudy._09_decorator._02_after;
+
+public class SpamFilteringCommentDecorator extends CommentDecorator {
+    public SpamFilteringCommentDecorator(CommentService commentService) {
+        super(commentService);
+    }
+
+    @Override
+    public void addComment(String comment) {
+        if (!isNotSpam(comment)) {
+            super.addComment(comment);
+        }
+    }
+
+    private boolean isNotSpam(String comment) {
+        return comment.contains("http");
+    }
+}
