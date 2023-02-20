522a7234f3aacabefab5f67e94d1083a4c464bfd
doukheeWon <fain9301@candh.co.kr>
2023-02-20 11:37:49 +0900

    feat: command Pattern

---
+++ src/main/java/kr/co/won/designpatternstudy/_14_command/_02_after/GameEndCommand.java
@@ -0,0 +1,20 @@
+package kr.co.won.designpatternstudy._14_command._02_after;
+
+public class GameEndCommand implements Command {
+
+    private Game game;
+
+    public GameEndCommand(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void execute() {
+        this.game.end();
+    }
+
+    @Override
+    public void undo() {
+        new GameStartCommand(this.game).execute();
+    }
+}
