aa1e0eae83b7c3ba281008550f282ce207eb98b9
doukheeWon <fain9301@candh.co.kr>
2023-02-21 12:49:47 +0900

    feat: set interpreter pattern

---
+++ src/main/java/kr/co/won/designpatternstudy/_15_interpreter/_03_java/Book.java
@@ -0,0 +1,18 @@
+package kr.co.won.designpatternstudy._15_interpreter._03_java;
+
+public class Book {
+
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
