971964c2b29c5350f9bffc3115da9788a5380750
doukheeWon <fain9301@candh.co.kr>
2023-02-28 11:01:29 +0900

    feat: add memento pattern

---
+++ src/main/java/kr/co/won/designpatternstudy/_18_memento/_02_after/GameSave.java
@@ -0,0 +1,22 @@
+package kr.co.won.designpatternstudy._18_memento._02_after;
+
+// Memento class이다.
+public class GameSave {
+
+    private final int blueTeamScore;
+
+    private final int redTeamScore;
+
+    public GameSave(int blueTeamScore, int redTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+}
---
+++ src/main/java/kr/co/won/designpatternstudy/_18_memento/package-info.java
@@ -0,0 +1,1 @@
+package kr.co.won.designpatternstudy._18_memento;
